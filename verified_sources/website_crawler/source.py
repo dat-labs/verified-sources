5b89a9d09df780a4955371f2cab632c23d3e6e9d
Rahul Kumar <rahul.kumar@decision-tree.com>
2024-04-09 20:38:27 +0530

    Added basic WebSite crawler files

---
+++ verified_sources/website_crawler/source.py
@@ -0,0 +1,6 @@
+from dat_core.connectors.sources.base import SourceBase
+
+class WebsiteCrawler(SourceBase):
+    def check_connection(self):
+        # Implement your connection check logic here
+        pass
