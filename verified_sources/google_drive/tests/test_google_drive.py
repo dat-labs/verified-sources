e5f45940478329c8130e1b3a9862453f0127c7f0
Rijumone Choudhuri <rijumone.choudhuri@decision-tree.com>
2024-08-28 13:56:13 +0530

    Tests need to be callable from anywhere but at least calling them from the root of the repo needs to be supported

--- verified_sources/google_drive/tests/test_google_drive.py
+++ verified_sources/google_drive/tests/test_google_drive.py
@@ -1,7 +1,6 @@
 from verified_sources.google_drive.source import GoogleDrive
-from verified_sources.google_drive.catalog import GoogleDriveCatalog
 from verified_sources.google_drive.specs import GoogleDriveSpecification
-from dat_core.pydantic_models import DatConnectionStatus, DatMessage, Type
+from dat_core.pydantic_models import DatConnectionStatus
 from datamodel_code_generator import InputFileType, generate, DataModelType
 from pathlib import Path
 from tempfile import TemporaryDirectory
@@ -30,7 +29,8 @@ def test_discover(valid_connection_object):
     assert isinstance(_d, dict)
 
 def test_model_files():
-    with open('specs.yml', 'r') as file:
+    parent = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+    with open(f'{parent}{os.path.sep}specs.yml', 'r') as file:
         datamodel = file.read()
     with TemporaryDirectory() as temporary_directory_name:
         temporary_directory = Path(temporary_directory_name)
@@ -42,12 +42,12 @@ def test_model_files():
             output_model_type=DataModelType.PydanticV2BaseModel,
         )
         model: str = output.read_text()
-        with open('tests/tmp_spec_model.py', 'w') as f:
+        with open(f'{parent}{os.path.sep}tests/tmp_spec_model.py', 'w') as f:
             f.write(model)
             
     from verified_sources.google_drive.tests.tmp_spec_model import GoogleDriveSpecification as GDSpec_temp
     assert compare_class_structures(GDSpec_temp, GoogleDriveSpecification)
-    os.remove('tests/tmp_spec_model.py')
+    os.remove(f'{parent}{os.path.sep}tests/tmp_spec_model.py')
 
 
 def compare_class_structures(class1, class2):
@@ -58,7 +58,6 @@ def compare_class_structures(class1, class2):
     # Filter out attributes that are automatically added by Python
     filtered_attrs_class1 = sorted({attr for attr in class1_attrs if not attr.startswith('__')})
     filtered_attrs_class2 = sorted({attr for attr in class2_attrs if not attr.startswith('__')})
-    
     # Compare the sets of attributes and methods
     if filtered_attrs_class1 == filtered_attrs_class2:
         return True
