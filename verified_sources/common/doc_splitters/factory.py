c391416656ffdf0c68d9e9814cb5a49094500e2f
Suryaansh Rai <suryaanshrai@gmail.com>
2024-10-15 13:53:09 +0530

    removed changes

--- verified_sources/common/doc_splitters/factory.py
+++ verified_sources/common/doc_splitters/factory.py
@@ -159,4 +159,3 @@ doc_splitter_factory.register_splitter(TextSplitterType.SPLIT_JSON_RECURSIVELY, 
 doc_splitter_factory.register_splitter(TextSplitterType.SPLIT_BY_CHARACTER_RECURSIVELY, RecursiveCharacterTextSplitter)
 # doc_splitter_factory.register_splitter('semantic_chunking', SemanticChunker)
 doc_splitter_factory.register_splitter(TextSplitterType.SPLIT_BY_TOKENS, RecursiveCharacterTextSplitter)
-
